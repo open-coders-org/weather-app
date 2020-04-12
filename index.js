284052fec68bf9242e2dbeb36891c861ad97a2b3
Edilson <eddylson.londonio@gmail.com>
2020-04-11 22:57:58 -0500

    auth client

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let minimist = require('minimist')
 let cTable = require('console.table')
 let {conf} = require ('./conf/conf')
 let {Weather} = require('./controller/wether-controller')
+let {create_user} = require('./controller/user-controller')
+let {cli} = require('cli-ux')
+let {login} = require('./auth/auth')
+
 
 let argv = minimist(process.argv.slice(2))
 
@@ -11,12 +15,23 @@ let {city, units} = argv
 
 async function main() {
     let weather = new Weather(url, appid)
+    let {create, username, pass } = argv
+    if (create && username &&  pass)  create_user(username,pass)
+    
 
+    let user = await cli.prompt('username')
+    let password = await cli.prompt('password',{type: 'hide'})
+
+    let login_response = await login(user,password)
+  
+     
+    if ( !login_response){
+        console.log('usuario o contraseña no coinciden');
+        process.exit(0)
+    }
+    
     try {
         let data = await weather.get_wather(city, units)
-        // console.log(data);
-
-
         let {temp, temp_min, temp_max, humidity} = data.data.main
         let {country} = data.data.sys
 
@@ -36,18 +51,14 @@ async function main() {
         
     } catch (error) {
         let {response:{data}} = error
-        //console.log(data);
 
         const table2 = cTable.getTable([
             data
         ])
         console.log(table2);
-        
-        
-        
-        
-
-    }
+        }
+    process.exit(0)
 }
 
 main()
+
